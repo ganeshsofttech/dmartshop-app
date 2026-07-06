659dfc0f485f5d53627a3aafb23913b9517cd1c5
ganeshsofttech <moreganesh327@gmail.com>
2026-07-06 11:43:56 +0000

    Missing code of CartItem Added

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,93 +1,33 @@
 export default function CartItem({
- 
-    item,
- 
-    increaseQuantity,
- 
-    decreaseQuantity,
- 
-    removeItem
- 
+  item,
+
+  increaseQuantity,
+
+  decreaseQuantity,
+
+  removeItem,
 }) {
- 
-    return (
- 
-        <div
- 
-            style={{
- 
-                border:"1px solid gray",
- 
-                padding:15,
- 
-                marginBottom:15
- 
-            }}
- 
-        >
- 
-            <h2>
- 
-                {item.name}
- 
-            </h2>
- 
-            <p>
- 
-                ₹{item.price}
- 
-            </p>
- 
-            <p>
- 
-                Quantity : {item.quantity}
- 
-            </p>
- 
-            <button
- 
-                onClick={()=>
- 
-                    decreaseQuantity(item.id)
- 
-                }
- 
-            >
- 
-                -
- 
-            </button>
- 
-            <button
- 
-                onClick={()=>
- 
-                    increaseQuantity(item.id)
- 
-                }
- 
-            >
- 
-                +
- 
-            </button>
- 
-            <button
- 
-                onClick={()=>
- 
-                    removeItem(item.id)
- 
-                }
- 
-            >
- 
-                Remove
- 
-            </button>
- 
-        </div>
- 
-    );
- 
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+
+        padding: 15,
+
+        marginBottom: 15,
+      }}
+    >
+      <h2>{item.name}</h2>
+
+      <p>₹{item.price}</p>
+
+      <p>Quantity : {item.quantity}</p>
+
+      <button onClick={() => decreaseQuantity(item.id)}>-</button>
+
+      <button onClick={() => increaseQuantity(item.id)}>+</button>
+
+      <button onClick={() => removeItem(item.id)}>Remove</button>
+    </div>
+  );
 }
