8fe33a92d367d043468ee093d8fba1dd85fee90b
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-06 12:50:45 +0530

    Add ProductList component to render product cards

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,26 @@
+import ProductCard from "./ProductCard";
+
+export default function ProductList({ products }) {
+
+   return (
+
+       <div>
+
+           {
+
+               products.map(product => (
+
+                   <ProductCard
+                       key={product.id}
+                       product={product}
+                   />
+
+               ))
+
+           }
+
+       </div>
+
+   );
+
+}
