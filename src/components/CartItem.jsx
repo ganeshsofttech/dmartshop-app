43ca692e86a22f4d8b19537ead129ee62f689243
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-06 17:12:19 +0530

    Implement CartItem component with item actions

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,93 @@
-export default function CartItem() {}
+export default function CartItem({
+ 
+    item,
+ 
+    increaseQuantity,
+ 
+    decreaseQuantity,
+ 
+    removeItem
+ 
+}) {
+ 
+    return (
+ 
+        <div
+ 
+            style={{
+ 
+                border:"1px solid gray",
+ 
+                padding:15,
+ 
+                marginBottom:15
+ 
+            }}
+ 
+        >
+ 
+            <h2>
+ 
+                {item.name}
+ 
+            </h2>
+ 
+            <p>
+ 
+                ₹{item.price}
+ 
+            </p>
+ 
+            <p>
+ 
+                Quantity : {item.quantity}
+ 
+            </p>
+ 
+            <button
+ 
+                onClick={()=>
+ 
+                    decreaseQuantity(item.id)
+ 
+                }
+ 
+            >
+ 
+                -
+ 
+            </button>
+ 
+            <button
+ 
+                onClick={()=>
+ 
+                    increaseQuantity(item.id)
+ 
+                }
+ 
+            >
+ 
+                +
+ 
+            </button>
+ 
+            <button
+ 
+                onClick={()=>
+ 
+                    removeItem(item.id)
+ 
+                }
+ 
+            >
+ 
+                Remove
+ 
+            </button>
+ 
+        </div>
+ 
+    );
+ 
+}
