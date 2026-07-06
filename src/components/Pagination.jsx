353cb003c4a5fbc0db3ba45cabc225b848130499
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-06 12:49:23 +0530

    Add Pagination component with navigation buttons

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,19 @@
+export default function Pagination() {
+
+   return (
+
+       <div>
+
+           <button>Previous</button>
+
+           <button>1</button>
+
+           <button>2</button>
+
+           <button>Next</button>
+
+       </div>
+
+   );
+
+}
