75b2052a6384fdc8874934a3dc07388af1e53217
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-06 12:48:31 +0530

    Add SearchBar component for product search

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+export default function SearchBar() {
+
+   return (
+
+       <div>
+
+           <input
+               type="text"
+               placeholder="Search products..."
+           />
+
+       </div>
+
+   );
+
+}
