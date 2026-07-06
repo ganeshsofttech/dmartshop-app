d5230204ac34b73f4ae537f69e0c8a10444214e0
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-06 12:49:01 +0530

    Add CategoryFilter component with category options

---
+++ src/components/CategoryFilter.jsx
@@ -0,0 +1,25 @@
+export default function CategoryFilter() {
+
+   return (
+
+       <div>
+
+           <select>
+
+               <option>All Categories</option>
+
+               <option>Fruits</option>
+
+               <option>Dairy</option>
+
+               <option>Bakery</option>
+
+               <option>Grains</option>
+
+           </select>
+
+       </div>
+
+   );
+
+}
